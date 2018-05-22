13261f52433f4ac71218b4acf86a363047c6c6e6
Shivakumar Burugu <burugu.shiva@gmail.com>
2018-05-21 22:36:49 -0700

    changes

--- app/src/main/java/android/com/aesexample/MainActivity.java
+++ app/src/main/java/android/com/aesexample/MainActivity.java
@@ -2,121 +2,68 @@ package android.com.aesexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    byte[] randomIV;
-    byte[] randomKey;
     private String TAG = MainActivity.class.getSimpleName();
+    public final String ZIGB_3_INPUT_MASK
+            = "[____]{ }[____]{ }[____]{ }[____]{ }[____]{ }[____]{ }[____]{ }[____]{ }[____]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.installation_code_input);
+        final String[] actualText = new String[1];
+        final String[] formattedText = new String[1];
+        final boolean[] edited = {false};
+        formattedText[0] = "";
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        String publicKey = "MIIEowIBAAKCAQEAtIcZYmflBfE6fRQAoGYsnUuFSNRZjjbCUUWgxkcBJeHepzFZ94Xk9+Y2Ed/xkO3ouUbvxFVZLWGzAjku0bCxoUAWKOKv53mp2SjhjQj5UYbTHknJ4/68Asyp25PszsVH8VnRJoPfrVjUNNArETpd6f8CxtpFgBFf66TUqT7LFPwEz4Xb0JPol68ap5ibP946Hwz2pTH5dIfmr/sW4XA8BIc16/Zto5Bn+hQOF6q/HJX8nOyyhnUUBg/AiOykc9fcc4jdj31Uvkbft+4S4DwoOCtDmhxAdgJMIbLsJv04qVeu1dhSRFS5sE443Qprd62Bn1lXlD5IsxfWvmSPNlEwxQIDAQABAoIBAByINbTd4s6bn7jhDnBLdcWuh/RIy5a0hOW/cXelBUNtfXlDTYrXeKbNlcIheNaFI4EU+xQ2wQ9xH4+8Ze/yU0C/FkGEAnU9P2D5GxzlTDJccsWYt/X27tQqcLYpnQXZHFPC9FF4CkQlg/SM6iGg7fLrwQ6YtN9W36gotwvAxuwb3J/2LZEy83Wse5WIlRpJuBsjoerZTPsGuF3Oufe+E8ZkUD7vgTauwVA44uRm6FVDThm7G4rq56nYK7RsBi0iGI0GzTnYaaBSnlUNgZMgfeJw81BAZJWCap2szxtB1QX/1pSz/MsmN3zBmM6ojmpt9IhhhoaLsXRCrzPeKSvFI4kCgYEA6VduMMIHLj7T3TOj7kAiBGuttx1X0R5xGEV5PMVE13BIPukPr2JMxeFvIgPRiefttH4uSwukl5Y3xYXH0HN62RqjGSOcnAPgpaEMzmLFtb2XrSIqHyOjvu59ncBXBclaiOTOW2QWX1GF0RicGHGfOZ3V8/zPgy6yF0pHGWkZn9sCgYEAxg7JLzMdiu2LYP41XUuwfCrNZwN/NblNo76cWiwvEAqhKtU7289HSofoOAUsLux/d8t5HupRfCvU4Ka3OeX19WFREwyVv638eqdRzhax/F3WOdsPnpSuIaMiAlNycCw9GMeaKk4NjKqVDtXx7k9Axn1vDPzbw/xpOKSr5JOsI98CgYBEo3FIamrOUbDkqqIYHUlkX4BUEw+X9gpyh00ocSbU8LRvvQLeJl5K/ws/yDvFAzJLG6rmbGBYLtntAnNKgA2s+0bqyCzzP7i/P+aSdz+NPF33kcsuKb+rFGBjHQb8LsSw7A0/mOtz0nk4jrKsBhA+VI1xHfa0sfG1JCZR9TaSbQKBgBABQeC+kLSZXZoI8OcJJsnh65dXgQAmaU/CJ4Bh8auqMDv131Bz/57WPL3KLTfdhK95keAdr1gJcrp0vnm/S5D4vER9z9wdgzmmK8Yrr1ZlftpOCYUXHrs5I5Zsh+LosEdAWgTxKcckQcdhIpvPa7yehkrWZCwCSb56ECn4hQChAoGBAIQHg1ZSVHCdfFHihwPGRuu10zviagBJp6wQveGVhiwMGBoiqW4LNGYV8294rxLlhOYaHhXqiyBobou1ge3lflcpu0S4zdCGwN1MgsTTc4PMp7AQHwSwTrXp2o+AULhRaUpmaCj4SmAfRX8+ov9ZIgiYlDL+C3TxPXItYjLYsisF";
+            }
 
-        String textToBeEncrypted = "{\n" +
-                "  \"ssid\": \"MV Office\",\n" +
-                "  \"psk\": \"i8some\"\n" +
-                "}";
-        //Step 1
-        try {
-            randomKey = AES256CipherUtil.getRandomAESCryptKey();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        //Step 2
-        randomIV = AES256CipherUtil.getRandomAesCryptIv();
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        //Step 3 replace need to replace this with the key coming from server
-        String encryptedSymmetricKey = null;
-        try {
-            encryptedSymmetricKey = RSA512CipherUtil.encryptText(new String(randomKey), RSA512CipherUtil.convertKeyStringtoPublicKey(publicKey));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
+            }
 
-        //Step 4
-        String encryptedSymmetricIV = null;
-        try {
-            encryptedSymmetricIV = RSA512CipherUtil.encryptText(new String(randomIV), RSA512CipherUtil.convertKeyStringtoPublicKey(publicKey));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //Log.i(TAG, "afterTextChanged:editText.getSelectionEnd() "+editText.getSelectionEnd());
+                //Log.i(TAG, "afterTextChanged:editText.getSelectionStart() "+editText.getSelectionStart());
+                //Log.i(TAG, "afterTextChanged: "+editable.toString());
+                if(edited[0]){
+                    //Log.i(TAG, "afterTextChanged: returning since override");
+                    editText.setSelection(formattedText[0].length());
+                    edited[0] = false;
+                    return;
+                }
+                if(formattedText[0].length() > editText.getMaxEms()){
+                    return;
+                }
+                actualText[0] = editable.toString();
+                if ((actualText[0].length() -1) > 0 && (actualText[0].length() + noOfSpaces(formattedText[0])) % 4 == 0) {
+                    formattedText[0] += actualText[0].charAt(actualText[0].length() - 1) + " ";
+                    edited[0] = true;
+                    Log.i(TAG, "afterTextChanged:formattedText[0] "+formattedText[0]);
+                    editText.setText(formattedText[0]);
+                } else if(editable.toString().length() < editText.getMaxEms()){
+                    formattedText[0] += actualText[0].charAt(actualText[0].length() - 1);
+                }
+            }
+        });
 
-        //Step 5
-        String convertedbase64payload = "";
-        try {
-            String encryptedWifiCredentials = AES256CipherUtil.encrypt(randomKey, randomIV, textToBeEncrypted);
-            convertedbase64payload = new String(Base64.encode(encryptedWifiCredentials.getBytes(), Base64.DEFAULT));
-            Log.i(TAG, "onCreate: convertedbase64payload " + convertedbase64payload);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        }
 
-        String finalPayload = "";
-        try {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("payload", convertedbase64payload);
-            jsonObject.put("symmetrickey", encryptedSymmetricKey);
-            jsonObject.put("iv", encryptedSymmetricIV);
-            finalPayload = jsonObject.toString();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        Log.i(TAG, "onCreate: Final payload " + finalPayload);
+    }
+    private int noOfSpaces(String text){
+        int counter = text.split(" ", -1).length - 1;
+        Log.i(TAG, "noOfSpaces: "+counter);
+        return counter;
     }
 }
